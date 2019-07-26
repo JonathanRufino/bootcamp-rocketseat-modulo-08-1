7c9eb7c0f9537a1fcbb14b116b2e7b9ed24882b7
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-07-25 21:22:20 -0300

    Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [techs, addTech] = useState(['ReactJS', 'React Native']);
+  const [techs, addTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     addTech([...techs, newTech]);
     setNewTech('');
   }
+
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      addTech(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
